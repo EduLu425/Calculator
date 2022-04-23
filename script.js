032f4e7e9e3ad4eed0dc4f7729ec55243551c3d8
Eduardo <ed.luna80@gmail.com>
2022-04-22 23:23:14 -0700

    Basic HTML structure and JS functions
    Define basic helper functions for each operate. Create operate function
    to run correct helper function based on chosen operator. Set up basic
    calculator structure and organization in html.

---
+++ script.js
@@ -0,0 +1,17 @@
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+const operate = function(operator, n1, n2) {
+    switch (operator) {
+        case '+': 
+            return add(n1, n2);
+        case '-':
+            return subtract(n1, n2);
+        case '*':
+            return multiply(n1, n2);
+        case '/':
+            return divide(n1, n2);
+    }
+}

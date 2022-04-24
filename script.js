a4bb604b1e4e081a1370bd5f92239053ba1c8cf8
Eduardo <ed.luna80@gmail.com>
2022-04-23 23:56:13 -0700

    Calculator working for one operator
    Program javascript to handle one operator at a time, however buttons
    have to be pressed in order and pressing operator a second time resets
    the calculator.

--- script.js
+++ script.js
@@ -5,13 +5,60 @@ const divide = (x, y) => x / y;
 
 const operate = function(operator, n1, n2) {
     switch (operator) {
-        case '+': 
+        case 'plus': 
             return add(n1, n2);
-        case '-':
+        case 'minus':
             return subtract(n1, n2);
-        case '*':
+        case 'multiply':
             return multiply(n1, n2);
-        case '/':
+        case 'divide':
             return divide(n1, n2);
     }
 }
+
+let operator = '';
+
+let n1 = 0;
+
+let n2 = 0;
+
+const numberButtons = document.querySelectorAll('.numBtn');
+
+const operatorButtons = document.querySelectorAll('.operator')
+
+const display = document.getElementById('numDisplay');
+
+const clearButton = document.getElementById('clear');
+
+const equalsButton = document.getElementById('equals');
+
+
+clearButton.addEventListener('click', () => {
+    display.value = '';
+    displayValue = [];
+})
+
+let displayValue = [];
+
+for (const button of numberButtons) {
+    button.addEventListener('click', function() {
+        displayValue.push(button.textContent);
+        display.value = displayValue.join('')
+
+    })
+}
+
+for (const button of operatorButtons) {
+    button.addEventListener('click', function() {
+        operator = button.id;
+        n1 = displayValue.join('');
+        display.value = '';
+        displayValue = [];
+    })
+}
+
+
+equalsButton.addEventListener('click', () => {
+    n2 = displayValue.join('');
+    display.value = operate(operator, n1, n2);
+})

b45bf26c9c37d3054351b2c3098c82a76e5ddb9d
Eduardo <ed.luna80@gmail.com>
2022-04-29 23:29:06 -0700

    Calculator bug fixes and improvements
    Add ability for calculator to evaluate multiple operations in order.

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+var operatorCounter = 0;
+var equalCounter = 0;
+var divZeroCounter = 0;
 
 const operate = function(operator, n1, n2) {
     switch (operator) {
@@ -36,6 +39,10 @@ const equalsButton = document.getElementById('equals');
 clearButton.addEventListener('click', () => {
     display.value = '';
     displayValue = [];
+    n1 = 0;
+    n2 = 0;
+    operatorCounter = 0;
+    equalCounter = 0;
 })
 
 let displayValue = [];
@@ -50,15 +57,48 @@ for (const button of numberButtons) {
 
 for (const button of operatorButtons) {
     button.addEventListener('click', function() {
-        operator = button.id;
-        n1 = displayValue.join('');
-        display.value = '';
-        displayValue = [];
+        operator = button.id;  
+        if (operatorCounter >= 1 && equalCounter === 0) {
+            n2 = parseInt(displayValue.join(''));
+            if (operator === 'divide' && n2 === 0) {
+                display.value = 'ERROR';
+                n1 = NaN;
+            }
+            else {
+                display.value = operate(operator, n1, n2)
+                n1 = parseInt(display.value);
+                displayValue = [];
+            }
+
+        }
+        else if (equalCounter === 1) {
+            equalCounter = 0;
+            display.value = '';
+            displayValue = [];
+            return
+        }
+        else {            
+            n1 = parseInt(displayValue.join(''));
+            display.value = '';
+            displayValue = [];
+            operatorCounter++;
+        }
+
     })
 }
 
 
+
 equalsButton.addEventListener('click', () => {
-    n2 = displayValue.join('');
-    display.value = operate(operator, n1, n2);
+    n2 = parseInt(displayValue.join(''));
+    equalCounter = 1;
+    if (operator === 'divide' && n2 === 0) {
+        display.value = 'ERROR';
+    }
+    else {
+        display.value = operate(operator, n1, n2);
+        n1 = parseInt(display.value);
+        n2 = 0;
+    }
+
 })
